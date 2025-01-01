3e0bb0e503f1134cedc8b70669a134b47126d4ec
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-02 00:45:50 +0200

    Add project files.

---
+++ Common1/TypeA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common1
+{
+    // What can you write inside the Namespace?
+    // 1. Class
+    // 2. Struct [Stands for Struture]
+    internal class TypeA
+    {
+    }
+}
