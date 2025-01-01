ebe1276ca6bcc0b4e68df0ac635a1e23fae07497
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-02 00:51:06 +0200

    Namespace Members (User-defined Types)

--- Common1/TypeA.cs
+++ Common1/TypeA.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Common1
 {
-    // What can you write inside the Namespace?
-    // 1. Class
-    // 2. Struct [Stands for Struture]
+    //// What can you write inside the Namespace?
+    //// 1. Class
+    //// 2. Struct [Stands for Struture]
+    //// 3. Interface
+    //// 4. Enum
+    //// 5. Delegate
+    //// 6. Record [C# 9.0 New feature (.NET 5.0)]
+    //// 7. Nested Namespace
+
+   // Allowed Access Modifiers inside the Namespace:
+   // 1. Internal [Default]
+   // 2. Public
+   // 3. File [C# 11.0 feature]
     internal class TypeA
     {
     }
