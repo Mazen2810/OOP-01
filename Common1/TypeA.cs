784535dae559bb5b0c13fd4b48250df40ff5e8e3
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-02 13:50:39 +0200

    Namespace Members (Class, struct)

--- Common1/TypeA.cs
+++ Common1/TypeA.cs
@@ -15,11 +15,48 @@ namespace Common1
     //// 6. Record [C# 9.0 New feature (.NET 5.0)]
     //// 7. Nested Namespace
 
-   // Allowed Access Modifiers inside the Namespace:
-   // 1. Internal [Default]
-   // 2. Public
-   // 3. File [C# 11.0 feature]
-    internal class TypeA
+    // Allowed Access Modifiers inside the Namespace:
+    // 1. Internal [Default]
+    // 2. Public
+    // 3. File [C# 11.0 feature]
+    public class TypeA
     {
+        // what you can write inside the class ?
+        // 1. Fields (Attributes | Member variables)
+        // 2. Properties (Full, Automatic)
+        // 3. Indexers (Special property)
+        // 4. Methods
+        // 5. Operators
+        // 6. Constructors
+        // 7. Destructors
+        // 8. Events
+        // 9. Static members (Static fields, static properties,static methods,static constructor)
+        // 10. Nested Types (Classes, structs,interfaces,enums and etc...)
+
+        // Allowed Access modifiers inside the Class:
+        // 1. Private
+        // 2. Private Protected
+        // 3. Protected
+        // 4. internal
+        // 5. internal protected
+        // 6. public
+    }
+
+    public struct TypeZ
+    {
+        // what you can write inside the struct ?
+        // 1. Fields (Attributes | Member variables)
+        // 2. Properties (Full, Automatic)
+        // 3. Indexers (Special property)
+        // 4. Methods
+        // 5. Operators
+        // 6. Constructors
+        // 7. Events
+        // 8. Nested Types (Classes, structs,interfaces,enums and etc...)
+
+        // Allowed Access modifiers inside the enum:
+        // 1. Private
+        // 2. internal
+        // 3. public
     }
 }
