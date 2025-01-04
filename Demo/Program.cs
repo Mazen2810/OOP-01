8de7ca397604147de3e7c7fb22aa5fd01bf25c94
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-04 14:57:02 +0200

    Enum Part 01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,11 +1,58 @@
 ﻿
 namespace Demo
 {
+    class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Salary { get; set; }
+        public Gender Gender { get; set; }
+    }
+    enum Gender
+    {
+        Male, Female
+    }
+    enum Grade
+    {
+        A, B, C, D, E, F  // Labels
+    }
+    enum Branch : byte
+    {
+        Dokki, Maadi, NasrCity, Alex, BNS, Smartvillage
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region ENum
+            #region Ex 01
+            /// Person person = new Person();
+            /// 
+            /// person.Id = 1001;
+            /// person.Name = "Mazen";
+            /// person.Salary = 10_000;
+            /// person.Gender = Gender.Male;
+            /// 
+            /// Gender mygender = Gender.Female;
+            /// Console.WriteLine(mygender);
+
+
+            Grade myGrade = Grade.A; // 4 Bytes
+            /// if(myGrade == Grade.A)
+            ///     Console.WriteLine(":)");
+            /// else
+            ///     Console.WriteLine(":(");
+
+
+            ///  myGrade = (Grade) 3;
+            ///  myGrade = (Grade) 255;
+            ///  Console.WriteLine(myGrade);
+            #endregion
+
+          
+            #endregion
+
         }
     }
 }
