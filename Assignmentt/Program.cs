175ec729ed56145b1325b9af35a34456f8f056ef
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-04 18:12:31 +0200

    Q3

--- Assignmentt/Program.cs
+++ Assignmentt/Program.cs
@@ -1,13 +1,19 @@
 ﻿namespace Assignmentt
 {
 
-    enum WeekDays
+    enum WeekDays :byte
     {
         Monday,Tuesday, Wednesday, Thursday , Friday, Saturday , Sunday
     }
-    enum Season
+    enum Season : byte
     {
         Spring =1, Summer, Autumn, Winter  // Spring = 1 because if the user enter invalid value, the tryparse will return 0
+    }
+
+    [Flags]
+    enum Permissions : byte
+    {
+        Read = 1, Write = 2 , Delete = 4 , Execute = 8
     }
     internal class Program
     {
@@ -23,7 +29,7 @@
             #endregion
 
             #region Q2: Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season
-            
+
             //Console.Write("Enter the Season: ");
             //Enum.TryParse<Season>(Console.ReadLine(), true, out Season result);
             //if(result == Season.Spring)
@@ -37,7 +43,25 @@
             //else
             //    Console.WriteLine("Enter a Valid Season");
 
-                
+
+            #endregion
+
+
+            #region Q3: Assign the following Permissions (Read, write, Delete, Execute) in a form of Enum.
+            //Permissions Permission;
+            //Permission = (Permissions)12; // Delete , Execute
+
+
+            //Permission = Permission | Permissions.Read;   // Add Permission "Read"
+            //Console.WriteLine(Permission); // Read , Delete , Execute
+
+            //Permission = Permission & ~(Permissions.Read);   // remove Permission "Read"
+            //Console.WriteLine(Permission);
+
+
+            //bool IsExist = (Permission & Permissions.Read) == Permissions.Read;  // Checking if "Read" exist
+            //Console.WriteLine(IsExist);
+
             #endregion
         }
     }
