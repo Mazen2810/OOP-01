cc4ff1f4dd0548ab2c110b0b65c8744cda53d376
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-04 17:17:57 +0200

    Q1

---
+++ Assignmentt/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignmentt
+{
+
+    enum WeekDays
+    {
+        Monday,Tuesday, Wednesday, Thursday , Friday, Saturday , Sunday
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 write a C# program that prints out all the days of the week using this enum.
+            //WeekDays Weekday;
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    Weekday = (WeekDays)i;
+            //    Console.WriteLine(Weekday);
+            //}
+            #endregion
+        }
+    }
+}
