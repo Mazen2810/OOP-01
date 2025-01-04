cad7ac7963ecb8c22f4f875055454d6234ea157e
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-04 17:40:46 +0200

    Q2

--- Assignmentt/Program.cs
+++ Assignmentt/Program.cs
@@ -5,6 +5,10 @@
     {
         Monday,Tuesday, Wednesday, Thursday , Friday, Saturday , Sunday
     }
+    enum Season
+    {
+        Spring =1, Summer, Autumn, Winter  // Spring = 1 because if the user enter invalid value, the tryparse will return 0
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -16,6 +20,24 @@
             //    Weekday = (WeekDays)i;
             //    Console.WriteLine(Weekday);
             //}
+            #endregion
+
+            #region Q2: Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season
+            
+            //Console.Write("Enter the Season: ");
+            //Enum.TryParse<Season>(Console.ReadLine(), true, out Season result);
+            //if(result == Season.Spring)
+            //    Console.WriteLine("From March to May");
+            //else if(result == Season.Summer)
+            //    Console.WriteLine("From June to August ");
+            //else if(result == Season.Autumn)
+            //    Console.WriteLine("From September to November ");
+            //else if(result == Season.Winter)
+            //    Console.WriteLine("From December to February ");
+            //else
+            //    Console.WriteLine("Enter a Valid Season");
+
+                
             #endregion
         }
     }
